4a50ed5e79575abb2a2520265a4f26bf0b617daf
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2018-11-13 22:06:00 +0100

    Defense of Plainswood solution

---
+++ forest/defense-of-plainswood.js
@@ -0,0 +1,5 @@
+// Build two fences on the marks to keep the villagers safe!
+// Hover your mouse over the world to get X,Y coordinates.
+
+hero.buildXY("fence", 40, 52);
+hero.buildXY("fence", 40, 20);
