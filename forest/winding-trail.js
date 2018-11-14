5ccc66d6d4b59516ade45ebef2756415b4915e87
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2018-11-14 22:54:00 +0100

    Winding Trail sollution

---
+++ forest/winding-trail.js
@@ -0,0 +1,9 @@
+// Go to the end of the path and build a fence on the X
+// Use your moveXY(x, y) function.
+
+// It's the first point of the path.
+hero.moveXY(36, 59);
+// Move at the next points of the path.
+hero.moveXY(37, 13);
+// Build a fence to stop the ogre.
+hero.buildXY("fence", 72, 25);
