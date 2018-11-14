22141b0810cbe27be3b35a3e4808371704c9d32e
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2018-11-14 02:12:21 +0100

    peace and love, collect money only

--- multiplayer/forest_multiplayer-treasure-grove.js
+++ multiplayer/forest_multiplayer-treasure-grove.js
@@ -1,72 +1,85 @@
 
-//The second tactic without equipment (max postion is #437 of 7965 Blue Team)
-// Focusing on a better currency only in left site,
-// the ability to find only one coin from equipment, 
-// so need is use funny steps inside.
-// min stuff: 
-//  - moveXY [ex: leather boots]
-//  - findNearestItem [ex: wooden glasses]
+//The third tactic wit equipment who retirve array items (need .findItems function)
+// We can sort it and choice better coin
+//
+// if we focus fast shoes and max heald (3861), and not fight, effect is good
+// max postion is: 
+//                 *  #27 of 28511 Read Team
+//                 *  #47 of 7964 Bue Team  
 
-function goaway() {
-    //ToDo
-    
-    //for x
-    if (hero.pos.x > 22)
-        deltaX = -8;
-    else if (hero.pos.x < 22)
-        deltaX = +7;
-    //for y
-    if (hero.pos.y > 45)
-        deltaY = -11;
-    else if (hero.pos.y < 25)
-        deltaY = +9;
-    var newx = hero.pos.x + deltaX;
-    var newy = hero.pos.y + deltaY;
-    //if(newx >)
-    //hero.say(deltaX + "#" + deltaY + "x "+hero.pos.x+ "y "+hero.pos.y);
-    go(newx, newy);
-}
-function go(hx, hy) {
-    // hero.say(hx + "#" + hy + "x "+hero.pos.x+ "y "+hero.pos.y);
-    if (hero.isReady("jump")) {
-        hero.jumpTo({
-            'x': hx,
-            'y': hy
-        });
-    } else {
-        hero.moveXY(hx, hy);
+//find coin of value sorted by distance to hero
+function findGold(number) {
+    var items = hero.findItems();
+    var near = 9999;
+    var nearIndex = 0;
+
+    for (var i = 0; i < items.length; ++i) {
+        if (items[i].value == number) {
+            if (near > hero.distanceTo(items[i])) {
+                near = hero.distanceTo(items[i]);
+                nearIndex = i;
+            }
+        }
     }
+
+    return items[nearIndex];
+
+}
+
+//return one better money
+function getBetterGold() {
+    var gold3 = findGold(3);
+    var gold2 = findGold(2);
+    var gold1 = findGold(1);
+
+    var cost3 = calculateCost(gold3);
+    var cost2 = calculateCost(gold2);
+    var cost1 = calculateCost(gold1);
+
+    if (cost3 <= cost2 && cost3 <= cost1)
+        return gold3;
+
+    if (cost2 <= cost3 && cost2 <= cost1)
+        return gold2;
+
+    if (cost1 <= cost3 && cost1 <= cost2)
+        return gold1;
+}
+
+//calculate cost to move to coint, so value and distace to coin
+function calculateCost(coin) {
+    var cost = 9999;
+
+    if (coin)
+        cost = hero.distanceTo(coin) / coin.value;
+
+    return cost;
+
 }
 
 
-var bronze = 0;
-var deltaX = -10;
-var deltaY = -10;
 var lastHelth = hero.maxHealth;
 while (true) {
-    //  Find coins and/or attack the enemy.
-    var enemy = hero.findNearestEnemy();
-    var item = hero.findNearestItem();
-    //hero.say(hero.maxHealt + "#"+ hero.healt);
-    if (lastHelth - 10 > hero.health && enemy) {
-        hero.attack(enemy);
-        item = hero.findNearestItem();    //lastHelth = hero.health;
-    } else if (lastHelth - 10 > hero.health && item) {
-        go(item.pos.x, item.pos.y);
+
+    //set love an peace and collect money only
+    var war = false;
+    if (lastHelth - 10 > hero.health && war) {
+        //  Find coins and/or attack the enemy.
+        var enemy = hero.findNearestEnemy();
+        if (enemy)
+            hero.attack(enemy);
     } else {
-        if (item) {
-            if (item.value > 1 && item.pos.x < 42) {
-                go(item.pos.x, item.pos.y);
-            } else if (bronze < 20 && item.pos.x < 36) {
-                go(item.pos.x, item.pos.y);
-                bronze += 1;
-            } else if (hero.distanceTo(item) < 30 && item.pos.x < 36) {
-                go(item.pos.x, item.pos.y);
-            } else {
-                goaway();
+        var gold = getBetterGold();
+        if (gold) {
+            if (hero.isReady("jump")) {
+                hero.jumpTo({
+                    'x': gold.pos.x,
+                    'y': gold.pos.y
+                });
             }
-        } else {
-            goaway();
+            else {
+                hero.move(gold.pos);
+            }
         }
     }
 }
