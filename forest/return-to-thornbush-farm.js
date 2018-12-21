64e6689e2d117573066268b129a5fdbe8968151c
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2018-12-21 23:19:37 +0100

    Return to Thornbush Farm Solution

---
+++ forest/return-to-thornbush-farm.js
@@ -0,0 +1,22 @@
+// The function maybeBuildTrap defines TWO parameters!
+function maybeBuildTrap(x, y) {
+    // Use x and y as the coordinates to move to.
+    hero.moveXY(x, y);
+    var enemy = hero.findNearestEnemy();
+    if(enemy) {
+        // Use buildXY to build a "fire-trap" at the given x and y.
+        hero.buildXY("fire-trap", x, y);
+    }
+}
+
+while(true) {
+    // This calls maybeBuildTrap, with the coordinates of the top entrance.
+    maybeBuildTrap(43, 50);
+    
+    // Now use maybeBuildTrap at the left entrance!
+    maybeBuildTrap(25, 34);
+    
+    // Now use maybeBuildTrap at the bottom entrance!
+    maybeBuildTrap(43, 20);
+
+}    
