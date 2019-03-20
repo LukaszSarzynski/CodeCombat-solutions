0225c4d7108dd94d997b06f1ab1c26d3ab34b86e
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2019-03-20 23:02:42 +0100

    True Names solution

---
+++ dungeon/true-names.js
@@ -0,0 +1,9 @@
+// Defend against "Brak" and "Treg"!
+// You must attack small ogres twice.
+
+hero.moveRight();
+hero.attack("Brak");
+hero.attack("Brak");
+hero.attack("Treg");
+hero.attack("Treg");
+
