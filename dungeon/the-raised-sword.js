ad4eb6b7c863431ee2bd6ab49092abf6acac9a15
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2019-03-22 23:07:03 +0100

    The Raised Sword solution

---
+++ dungeon/the-raised-sword.js
@@ -0,0 +1,14 @@
+// Defeat the ogres.
+// Remember that they each take two hits.
+
+hero.attack("Rig");
+hero.attack("Rig");
+
+
+hero.attack("Gurt");
+hero.attack("Gurt");
+
+
+
+hero.attack("Ack");
+hero.attack("Ack");
