f556f2f540dc91f1771697644c75e33c003de6fc
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2019-03-23 23:08:53 +0100

    Kithgard Librarian solution

---
+++ dungeon/kithgard-librarian.js
@@ -0,0 +1,10 @@
+// You need to say a password to open the Library Door!
+// The password is in the Hints!
+// Click the blue Hints button above the code window.
+// If you ever get stuck, click Hints!
+hero.moveRight();
+hero.say("Hush");
+// ∆
+while (true) {
+    hero.moveRight();
+}
