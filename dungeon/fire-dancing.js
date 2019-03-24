51969060810755e41bd28ae0d81ebc3ae2985acf
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2019-03-24 23:14:13 +0100

    Fire Dancing solution

---
+++ dungeon/fire-dancing.js
@@ -0,0 +1,8 @@
+// Code normally executes in the order it's written.
+// Loops repeat a block of code multiple times.
+
+while(true) {
+    hero.moveRight();
+    // Add a moveLeft command to the loop here
+    hero.moveLeft();    
+}
