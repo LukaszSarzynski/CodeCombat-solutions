de55b2c0c44a59a01086f5fd686f982eb09adeee
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2019-03-24 23:12:41 +0100

    The Prisoner solution

---
+++ dungeon/the-prisoner.js
@@ -0,0 +1,9 @@
+// Free the prisoner, defeat the guard and grab the gem.
+
+// Free Patrick from behind the "Weak Door".
+hero.attack("Weak Door");
+// Defeat the guard, named "Two".
+hero.moveRight(3);
+hero.moveDown(3);
+hero.attack("Two");
+// Get the gem.
