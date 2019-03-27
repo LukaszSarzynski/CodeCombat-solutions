6ffb04115b04eefb84c4b8b7d81354bd4b9bacc8
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2019-03-27 23:24:24 +0100

    Cupboards of Kithgard solution

---
+++ dungeon/cupboards-of-kithgard.js
@@ -0,0 +1,11 @@
+// There may be something around to help you!
+
+// First, move to the Cupboard.
+hero.moveUp();
+hero.moveRight(2);
+hero.moveDown(2);
+// Then, attack the "Cupboard" inside a while-true loop.
+while(true)
+{
+    hero.attack("Cupboard");
+}
