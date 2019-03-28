7f9a3a548a57e2151bb1c1df8345dc7445d6a82b
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2019-03-28 23:26:26 +0100

    Known Enemy solution

---
+++ dungeon/known-enemy.js
@@ -0,0 +1,14 @@
+// You can use a variable like a nametag.
+
+var enemy1 = "Kratt";
+var enemy2 = "Gert";
+var enemy3 = "Ursa";
+
+hero.attack(enemy1);
+hero.attack(enemy1);
+
+hero.attack(enemy2);
+hero.attack(enemy2);
+
+hero.attack(enemy3);
+hero.attack(enemy3);
