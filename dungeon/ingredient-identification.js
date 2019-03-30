4a8d4272e0a250fb2e86ca8e04b3ae52ccb6f28d
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2019-03-30 23:29:44 +0100

    Ingredient Identification solution

---
+++ dungeon/ingredient-identification.js
@@ -0,0 +1,15 @@
+// A variable is a container with a label that holds data.
+
+// This variable is named `someVariableName`
+// It contains the value `"a string"`
+var someVariableName = "a string";
+
+// This variable is named `lolol`
+// It contains the number `42`
+var lolol = 42;
+
+// Create 2 more variables and assign values to them:
+// You can name them whatever you want, and hold any value in them!
+// Use `=` to assign a value to a variable.
+var oBlueBootle = "lolol";
+var oRedBootle = "someVariableName";
