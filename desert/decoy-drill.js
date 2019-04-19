e20526cc6f6da7c6bc76fce2f721d540b82c72f7
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2019-04-19 22:44:03 +0200

    Decoy Drill Solution

---
+++ desert/decoy-drill.js
@@ -0,0 +1,31 @@
+// We are field testing a new battle unit: the decoy.
+// Build 4 decoys, then report the total to Naria.
+var value = 0;
+var decoysBuilt = 0;
+while(true) {
+    var coin = hero.findNearestItem();
+    if(coin) {
+        value += coin.value;
+        // Collect the coin!
+        hero.moveXY(coin.pos.x, coin.pos.y);        
+    }
+    // Each decoy costs 25 gold.
+    // If hero.gold is greater than or equal to 25:
+    if(hero.gold >= 25)
+    {
+        // buildXY a "decoy"
+        // Add 1 to the decoysBuilt count.
+        hero.buildXY("decoy", hero.pos.x, hero.pos.y);
+        decoysBuilt++;
+    }            
+    
+    if(decoysBuilt == 4) {
+        // Break out of the loop when you have built 4.
+        break;
+    }
+}
+
+hero.say("Done building decoys!");
+hero.moveXY(14, 36);
+// Say how many decoys you built.
+hero.say(decoysBuilt);
